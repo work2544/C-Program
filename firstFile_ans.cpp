8fb8ba0a4bd6ae44f1b3692090340fd5b75106c0
work2544 <work21_123@hotmail.com>
2021-02-22 11:17:37 +0700

    version 0.5

---
+++ firstFile_ans.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"workza_ans";
+return 0;
+}
--- mainprogram.cpp
+++ mainprogram.cpp
@@ -9,12 +9,18 @@ int main() {
     ofstream fileWriter;
     string str;
     fileWriter.open ("firstFile.cpp");//stagename
+    fileWriter <<"#include<iostream>\n";
+    fileWriter <<"using namespace std;";
+    fileWriter <<"int main(){\n";
     while (getline(cin,str)&&str!="exit")//writcode
     {
     fileWriter << str << endl;
     } 
+    fileWriter <<"system(\"g++ firstFile_ans.cpp\");\n";//calcompilerer
+    fileWriter <<"system(\"firstFile_ans.exe\");\n";//run
+    fileWriter <<"return 0;}\n";
     fileWriter.close();
-
     system("g++ firstFile.cpp");//calcompilerer
     system("a.exe");//run
+    return 0;
 }
